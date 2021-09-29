ab9b0fe065d911747a33354aec91c40a17fc04b0
Joe <24jdburks@macon.k12.mo.us>
2021-09-29 10:50:12 -0500

    first push

---
+++ MyHelloWorldJB/src/myhelloworldjb/MyHelloWorldJB.java
@@ -0,0 +1,15 @@
+
+package myhelloworldjb;
+
+/**
+ *
+ * @author maconrm180
+ */
+public class MyHelloWorldJB {
+//My Hello World- Joe Burks- September 2021
+   
+    public static void main(String[] args) {
+        System.out.println("Hello to you world!");
+    }
+    
+}
